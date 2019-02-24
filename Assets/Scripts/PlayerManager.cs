84d16eb259512efb8b4bf4c80a8f2bf925e7f2fd
Kian Murdock <kmurdoc4@uncc.edu>
2019-02-24 03:36:30 -0500

    character controller basics
    basic walking/crouch walking with camera

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    [SerializeField] private Animator animator;
+    private int speedFloatHash;
+    private int crouchBoolHash;
+    private bool crouched = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        speedFloatHash = Animator.StringToHash("speed");
+        crouchBoolHash = Animator.StringToHash("crouch");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            animator.SetFloat(speedFloatHash, 1f);
+        }
+        else
+        {
+            animator.SetFloat(speedFloatHash, 0f);
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Crouch();
+        }
+    }
+
+    void Crouch()
+    {
+        animator.SetBool(crouchBoolHash, !crouched);
+        crouched = !crouched;
+    }
+}
---
+++ Assets/Scripts/TopCameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopCameraController : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        transform.position = desiredPosition;
+
+        transform.LookAt(target.transform.position);
+    }
+}
